3bcbab0dd75e6de17055d9c9738766398ce5c137
leo77544 <2510176071@qq.com>
2019-03-15 15:19:13 +0800

    测试存储过程

--- H_PMS_WebApi/H_PMS_DAL/LeoService.cs
+++ H_PMS_WebApi/H_PMS_DAL/LeoService.cs
@@ -7,10 +7,37 @@ using System.Data.SqlClient;
 
 namespace H_PMS_DAL
 {
-    public class RepairService
+    public class LeoService
     {
-        
-
+        /// <summary>
+        /// 添加收费信息
+        /// </summary>
+        /// <param name="m"></param>
+        /// <returns></returns>
+      static  public int AddDataMoney(DataMoney m)
+        {
+            SqlParameter DMNumber = new SqlParameter("@DMNumber", SqlDbType.VarChar);
+            DMNumber.Value = m.DMNumber;
+            SqlParameter HostName = new SqlParameter("@HostName", SqlDbType.VarChar);
+            HostName.Value = m.HostName;
+            SqlParameter DMSTime = new SqlParameter("@DMSTime", SqlDbType.DateTime);
+            DMSTime.Value = m.DMSTime;
+            SqlParameter DMName = new SqlParameter("@DMName", SqlDbType.VarChar);
+            DMName.Value = m.DMName;
+            SqlParameter DMWay = new SqlParameter("@DMWay", SqlDbType.VarChar);
+            DMWay.Value = m.DMWay;
+            SqlParameter DMType = new SqlParameter("@DMType", SqlDbType.VarChar);
+            DMType.Value = m.DMType;
+            SqlParameter DMSum = new SqlParameter("@DMSum", SqlDbType.Float);
+            DMSum.Value = m.DMSum;
+            SqlParameter Remark = new SqlParameter("@Remark", SqlDbType.VarChar);
+            Remark.Value = m.Remark;
+            SqlParameter pcode = new SqlParameter("@Code", SqlDbType.Int);
+            pcode.Direction = ParameterDirection.Output;
+            SqlParameter[] para = { DMNumber, HostName, DMSTime , DMName, DMWay, DMType , DMSum, Remark,pcode };
+             DBHelperProc.ExecuteNonQuery("P_dataMomey", para);
+            return Convert.ToInt32(pcode);
+        }
     }
 }
 
