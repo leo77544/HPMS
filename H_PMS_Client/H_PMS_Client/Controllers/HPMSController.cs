aeef03a8c9cc9cc616593cc9c1abc8203a9dc249
TwoPillars <897050227@qq.com>
2019-03-18 19:53:31 +0800

    AlanOneDay

--- H_PMS_Client/H_PMS_Client/Controllers/HPMSController.cs
+++ H_PMS_Client/H_PMS_Client/Controllers/HPMSController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using WebApiHelper;
 using H_PMS_Model;
+using Newtonsoft.Json;
+
 namespace H_PMS_Client.Controllers
 {
     public class HPMSController : Controller
@@ -15,6 +17,33 @@ namespace H_PMS_Client.Controllers
          //   ApiResult.GetAPIResult();
             return View();
         }
-
+        public ActionResult PBAddIndex()
+        {
+            return PartialView();
+        }
+        public ActionResult PBShow()
+        {
+            return PartialView();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public int Login(string name ,string pwd)
+        {
+            string json = WebApiHelper.ApiResult.GetAPIResult("Login/?account=" + name + "&pwd=" + pwd, "get");
+            Employee emp = JsonConvert.DeserializeObject<Employee>(json);
+            
+            if (emp != null)
+            {
+                Session["TheU"] = emp;
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
     }
 }
